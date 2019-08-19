eed3307f64782dc4897b76298244b3fe1b910b78
Felix <13115412392@163.com>
2019-08-19 10:19:56 +0800

    播放音乐

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
-const {app, BrowserWindow, ipcMain, dialog} = require('electron')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 const DataStore = require('./renderer/MusicDataStore')
 
 const myStore = new DataStore({'name': 'Music Data'})
-class AppWindow extends BrowserWindow{
-  constructor(config,fileLocation){
-    const basicConfig={
+class AppWindow extends BrowserWindow {
+  constructor(config, fileLocation) {
+    const basicConfig = {
       width: 800,
       height: 600,
-      webPreferences:{
+      webPreferences: {
         nodeIntegration: true
       }
     }
-    const finalConfig = {...basicConfig,...config}//es6 展开对象合并
-    super (finalConfig)
+    const finalConfig = { ...basicConfig, ...config }
+    super(finalConfig)
     this.loadFile(fileLocation)
-    this.once('ready-to-show',() => {
+    this.once('ready-to-show', () => {
       this.show()
     })
   }
 }
-app.on('ready',() => {
-  const mainWindow = new AppWindow({},'./renderer/index.html')
-  ipcMain.on('add-music-window',() => {
+app.on('ready', () => {
+  const mainWindow = new AppWindow({}, './renderer/index.html')
+  mainWindow.webContents.on('did-finish-load',() => {
+    mainWindow.send('getTracks', myStore.getTracks())
+  })
+  ipcMain.on('add-music-window', () => {
     const addWindow = new AppWindow({
       width: 500,
       height: 400,
-      parent:mainWindow
-    },'./renderer/add.html')
+      parent: mainWindow
+    }, './renderer/add.html')
   })
-  ipcMain.on('add-tracks',(event,tracks)=>{
+  ipcMain.on('add-tracks', (event, tracks) => {
     const updatedTracks = myStore.addTracks(tracks).getTracks()
-    console.log(updatedTracks)
+    mainWindow.send('getTracks', updatedTracks)
+  })
+  ipcMain.on('delete-track', (event, id) => {
+    const updatedTracks = myStore.deleteTrack(id).getTracks()
+    mainWindow.send('getTracks', updatedTracks)
   })
   ipcMain.on('open-music-file', (event) => {
     dialog.showOpenDialog({
